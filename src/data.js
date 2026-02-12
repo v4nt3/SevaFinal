468bdaea0b218458da94a28f767e9f0f4416555a
Gissel Quitian <gisselvanessaq@yahoo.es>
2026-02-11 23:13:58 -0500

    correcion de nombres y modelo de bruja

--- src/data.js
+++ src/data.js
@@ -8,21 +8,21 @@ export const productos = [
   },
   {
     id: 2,
-    nombre: "Picaro",
+    nombre: "Ficha de Picaro",
     precio: 500000,
     modelo: "/model/picaro.glb",
     descripcion: "Pícaro ágil y sigiloso que se mueve en las sombras del calabozo, desactiva trampas y ataca por sorpresa cuando el enemigo menos lo espera"
   },
   {
     id: 3,
-    nombre: "Bruja",
+    nombre: "Ficha de Bruja",
     precio: 120000,
     modelo: "/model/bruja.glb",
     descripcion: "Bruja misteriosa que domina la magia oscura, lanza hechizos poderosos desde la distancia y controla energías ocultas para derrotar a sus enemigos en el calabozo"
   },
   {
     id: 4,
-    nombre: "Sacerdote",
+    nombre: "Ficha de Sacerdote",
     precio: 30000,
     modelo: "/model/sacerdote.glb",
     descripcion: "Sacerdote sagrado que protege a los guerreros con hechizos de sanación y defensa. Su presencia inspira coraje y fortaleza en el calabozo."
